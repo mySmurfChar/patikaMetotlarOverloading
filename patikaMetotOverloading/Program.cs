4767f25b8bfcd0b5d8e4f26c4a8108f70aa9829c
MuhammetCanLikya <158784308+mySmurfChar@users.noreply.github.com>
2024-02-08 23:42:10 +0300

    first commit

---
+++ patikaMetotOverloading/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace patikaMetotOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi = "999";
+            int outSayi;
+            bool sayiTespit = int.TryParse(sayi, out outSayi);
+            if (sayiTespit )
+            {
+                Console.WriteLine("işlem başarılı");
+                Console.WriteLine(outSayi.GetType);
+                Console.WriteLine(sayi);
+            }
+            else
+            {
+                Console.WriteLine("işlem başarısız");
+            }
+            Topla(4, 5, out int toplam);
+            Console.WriteLine(toplam);
+            ekranaYazdir(12);
+            ekranaYazdir("Merhaba Dünya");
+
+
+        }
+
+        public static void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public static void ekranaYazdir(string a)
+        {
+            Console.WriteLine(a);
+        }
+        public static void ekranaYazdir(int a)
+        {
+            Console.WriteLine(a);
+        }
+
+    }
+}
